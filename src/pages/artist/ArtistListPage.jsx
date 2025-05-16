5085711ad8f621dc7fc736bade9a253dae5c787d
SimoneChiodo <simochi.005@gmail.com>
2025-05-16 17:22:18 +0200

    Change port from 5173 to 5000; Add fetch to backend in VynilListPage and ArtistListPage

--- src/pages/artist/ArtistListPage.jsx
+++ src/pages/artist/ArtistListPage.jsx
@@ -1,8 +1,34 @@
+import { useEffect, useState } from "react";
+
 export default function ArtistListPage() {
+  const { VITE_BACKEND_API_URL } = import.meta.env;
+
+  let [artistList, setArtistList] = useState([]);
+
+  const fetchArtist = () => {
+    fetch(`${VITE_BACKEND_API_URL}/artist`)
+      .then((res) => res.json())
+      .then((data) => {
+        setArtistList(data);
+      });
+  };
+
+  useEffect(() => {
+    fetchArtist();
+  }, []);
+
   return (
     <>
       <div className="container">
         <h1>ArtistListPage</h1>
+
+        <ul>
+          {artistList.map((artist) => (
+            <li key={artist.id}>
+              <a href={`/artist/${artist.id}`}>{artist.name}</a>
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   );
--- src/pages/vynil/VynilListPage.jsx
+++ src/pages/vynil/VynilListPage.jsx
@@ -1,11 +1,34 @@
+import { useEffect, useState } from "react";
+
 export default function VynilListPage() {
   const { VITE_BACKEND_API_URL } = import.meta.env;
+
+  let [vynilList, setVynilList] = useState([]);
+
+  const fetchVynils = () => {
+    fetch(`${VITE_BACKEND_API_URL}/vynil`)
+      .then((res) => res.json())
+      .then((data) => {
+        setVynilList(data);
+      });
+  };
+
+  useEffect(() => {
+    fetchVynils();
+  }, []);
 
   return (
     <>
       <div className="container">
         <h1>VynilListPage</h1>
-        <p>{VITE_BACKEND_API_URL}</p>
+
+        <ul>
+          {vynilList.map((vynil) => (
+            <li key={vynil.id}>
+              <a href={`/vynil/${vynil.id}`}>{vynil.name}</a>
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   );
