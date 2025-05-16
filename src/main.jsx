94ba57d65f87c5c219b40eda5f4dcf5bec6b0dd5
SimoneChiodo <simochi.005@gmail.com>
2025-05-16 16:24:23 +0200

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+// import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+// Components
+import App from "./App.jsx";
+
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// Bootstrap JS
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+// Custom CSS
+import "./assets/style.css";
+
+createRoot(document.getElementById("root")).render(
+  // <StrictMode>
+  <App />
+  // </StrictMode>
+);
