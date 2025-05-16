ffd2307930a5f920a71f40d4466f555f72c57201
SimoneChiodo <simochi.005@gmail.com>
2025-05-16 16:37:25 +0200

    Add basic routes, basic pages, DefaultLayout, Navbar, Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-// Custom CSS
-import "./assets/style.css";
+// Default Layout
+import DefaultLayout from "./layout/DefaultLayout";
+
+// Pages
+import HomePage from "./pages/home/HomePage";
+import VynilListPage from "./pages/vynil/VynilListPage";
+import VynilDetailsPage from "./pages/vynil/VynilDetailsPage";
+import ArtistListPage from "./pages/artist/ArtistListPage";
+import ArtistDetailsPage from "./pages/artist/ArtistDetailsPage";
 
 function App() {
   return (
     <>
-      <h1>Vite + React</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route Component={DefaultLayout}>
+            <Route index element={<HomePage />}></Route>
+            <Route path="/vynil" element={<VynilListPage />}></Route>
+            <Route path="/vynil/:id" element={<VynilDetailsPage />}></Route>
+            <Route path="/artist" element={<ArtistListPage />}></Route>
+            <Route path="/artist/:id" element={<ArtistDetailsPage />}></Route>
+            <Route path="*" element={""}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
